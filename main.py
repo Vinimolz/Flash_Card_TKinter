72de1dcbe51d4eaf6126b3ec8a047c1c8301a847
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-03-27 21:13:36 -0700

    Creating first UI for Flash Cards

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flash Card App")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+#Creating main canvas that will contain the card promtp
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_image)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+#creating the "unknown word" button
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+#creating the "correct" button
+check_image = PhotoImage(file="images/right.png")
+correct_button = Button(image=check_image, highlightthickness=0)
+correct_button.grid(row=1, column=1)
+
+
+window.mainloop()
 
